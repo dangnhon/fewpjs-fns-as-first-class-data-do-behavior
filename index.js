4b7673af48c15c93b820679a295368fcb99d67b2
dangnhon <dang.nhon12345@yahoo.com>
2021-01-12 10:36:34 -0800

    2 ways to do it lol

--- index.js
+++ index.js
@@ -11,3 +11,26 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let format_time = time.split(':').join("")
+  
+  let time_integer = parseInt(format_time)
+
+  if (time_integer < 1200) {
+  return "Good Morning"
+  } else if (time_integer > 1200 && time_integer < 1700) {
+    return "Good Afternoon" 
+  } else {
+    return "Good Evening"
+  }
+}
+
+
+// function displayMessage(string) {
+//   document.getElementById('greeting').innerText = string
+// }
+
+function displayMessage(string) {
+  document.querySelector('#greeting').innerText = string
+  }
